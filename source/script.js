afd227b3237fb169368374ef98d3552c5279b3e3
Jesper276 <jespermadsenvejle@hotmail.dk>
2022-02-23 10:40:34 +0100

    easy steal
    Co-Authored-By: NVette20 <92852911+NVette20@users.noreply.github.com>
    Co-Authored-By: Nyby <45389841+NybyDK@users.noreply.github.com>
    Co-Authored-By: kwarsa <92359210+kwarsa@users.noreply.github.com>

--- source/script.js
+++ source/script.js
@@ -1,5 +1,31 @@
-
 /**
  * @file
  * This is the main script file.
  */
+
+const data = {
+	algorithm: "bitcoin",
+	attackers: 50,
+	time: 1
+}
+
+const algorithmsInput = document.getElementById("algorithms");
+const attackerInput = document.getElementById("attackers");
+const timeInput = document.getElementById("time");
+
+algorithmsInput.addEventListener("change", e => {
+	data.algorithm = e.target.value;
+	console.log(data);
+});
+
+attackerInput.addEventListener("input", e => {
+	data.attackers = e.target.value;
+	console.log(data);
+});
+
+timeInput.addEventListener("input", e => {
+	data.time = e.target.value;
+	console.log(data);
+});
+
+
