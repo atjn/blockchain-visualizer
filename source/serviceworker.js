b1776bd5b4f90e17d04df8756aaabb4080549766
atjn <dev@atjn.dk>
2022-03-31 17:13:29 +0200

    wip

--- source/serviceworker.js
+++ source/serviceworker.js
@@ -36,6 +36,7 @@ self.addEventListener("fetch", event => {
 	event.respondWith(
 		caches.match(
 			event.request,
+			{ ignoreSearch: true },
 		).then(response => {
 			if(response){
 				return response;
