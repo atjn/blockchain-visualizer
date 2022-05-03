49f749778827d0c5190dbd8317b207845c3cff8e
Skrome <anders.nyby01@gmail.com>
2022-05-03 12:00:51 +0200

    day 1 lol

--- source/scripts/simulation.js
+++ source/scripts/simulation.js
@@ -20,6 +20,7 @@
 
 import { Packet, AddressPacket, NodeData, sendDrawEvent, random, NewBlockSignal, BlockChain, sendErrorEvent, sendLogEvent } from "./nodeMethods.js";
 import { average, clampMin } from "./utilities.js";
+import { Nodes } from "./simulationMethods.js";
 
 
 /**
@@ -383,42 +384,6 @@ class NodeEvent extends SimulationEvent{
 		}
 
 	}
-}
-
-/**
- * This is the store of all the node's permanent data, linked to their addresses.
- *
- * Node data is saved here, until a `NodeProcess` with a specific address runs, in which case the data
- * for that address is taken out, and then later delivered back when the `NodeProcess` has altered it.
- */
-class Nodes extends Map{
-
-	create(){
-		const newNode = new NodeData();
-		super.set(newNode.address, newNode);
-
-		sendDrawEvent({
-			type: "node",
-			address: newNode.address,
-			position: newNode.position,
-		});
-
-		return newNode.address;
-	}
-
-	/**
-	 * Save a new/modified data object for a specific node address.
-	 *
-	 * @param {NodeData} newData - The new data object to save.
-	 * @param {number} timestamp - When the data was saved, according to simulation time.
-	 *
-	 * @returns {Nodes} - This.
-	 */
-	update(newData){
-		// Finally, save the new data.
-		return super.set(newData.address, newData);
-	}
-
 }
 
 /**
---
+++ test/algorithms.js
@@ -0,0 +1,60 @@
+import tap from "tap";
+import fs from "fs-extra";
+import path from "path";
+import { fileURLToPath } from "url";
+const site_root = path.join(path.dirname(fileURLToPath(import.meta.url)), "../");
+const algorithms_path = path.join(site_root, "source/scripts/algorithms");
+
+import { webcrypto } from "crypto";
+globalThis.crypto = webcrypto;
+
+import { AddressPacket, Block, BlockPacket, NewBlockSignal } from "../source/scripts/nodeMethods.js";
+import { Nodes } from "../source/scripts/simulationMethods.js";
+
+globalThis.settings = {
+	aspectRatio: 1,
+	network: {
+		delay: 1
+	}
+}
+globalThis.nodes = new Nodes();
+
+for (const file of fs.readdirSync(algorithms_path)) {
+	const node = await import("file://" + path.join(algorithms_path, file));
+
+	const to = globalThis.nodes.create();
+	const addresses = [globalThis.nodes.create(), globalThis.nodes.create(), globalThis.nodes.create()];
+
+	const packet = new AddressPacket(to, globalThis.nodes.create(), addresses);
+
+	const { nodeData, sendPackets } = await node.process(packet, globalThis.nodes.get(to));
+
+	tap.ok(addresses.every(address => nodeData.hasAddress(address)), "node has all addresses");
+
+	tap.ok(sendPackets.length > 0, "node is sending outbound packets");
+
+
+
+	const block = new Block();
+	const blockPacket = new BlockPacket(to, globalThis.nodes.create(), block);
+
+	node.process(blockPacket, globalThis.nodes.get(to));
+
+	tap.ok(nodeData.blockchain.has(blockPacket.block), "node has added the block to its blockchain");
+
+
+
+	const newBlockSignal = new NewBlockSignal(to, globalThis.nodes.create(), block);
+
+	const { sendPackets: sendNewBlockPackets} = await node.process(newBlockSignal, globalThis.nodes.get(to));
+
+	tap.ok(sendNewBlockPackets.length > 0, "new block is being sent out");
+
+	tap.ok(sendNewBlockPackets.every(block => block instanceof BlockPacket), "outbound packets from new block signal are block packets");
+}
+
+// hasAddress has all addresses
+// sendPackets is not empty
+// nodedata blockchain gets new block
+// new blocks are sent yes
+// are the blocks sent out the correct block???
