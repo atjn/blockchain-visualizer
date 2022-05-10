9dcb8722f9941f4f2a19bfeaa324fd4d675abca9
Anton <dev@atjn.dk>
2022-05-10 08:45:39 +0200

    Improve documentation of build files (#39)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,8 @@
 
 /**
  * @file
- * This file confgures ESLint.
+ * This file confgures ESlint to use atjn's preferred settings.
+ * ESlint makes sure that all JavaScript is formatted in a safe and somewhat standardized way.
  */
 
 module.exports = {
